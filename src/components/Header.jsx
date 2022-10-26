d42799bed4619b7cdec2e02c897c918408e1bff1
Emmanuel uzoezie <mac@macs-MacBook-Pro.local>
2022-10-27 00:33:09 +0100

    added the footer component, logo, favorite page and settings page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,26 @@
 import { GiHamburgerMenu } from "react-icons/gi"
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ReactComponent as PersonOutlineIcon } from "../assets/svg/personOutlineIcon.svg";
 import "./Header.css"
-import banner from "../data/banner.png"
 import { useStateContext } from "../context/ContextProvider"
+import logo from "../data/logo.png"
 
 const Header = () => {
-    const { setSidebar } = useStateContext()
-    const navigate = useNavigate();
-  const onClick =()=>{
-     navigate('/sign-up')
+  const { setSidebar } = useStateContext()
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    navigate('/signup')
   }
   return (
     <div className="header">
-        <div className="header-items">
-              <GiHamburgerMenu onClick={() => setSidebar(true)} style={{ cursor:"pointer"}}/>
-              <h1 style={{ cursor: "pointer" }}>Logo</h1>
-              <div className="header-right">
-              <PersonOutlineIcon onClick={onClick} style={{cursor:"pointer"}}/>
-              </div>
+      <div className="header-items">
+        <GiHamburgerMenu onClick={() => setSidebar(true)} style={{ cursor: "pointer" }} />
+        <Link to="/"><img src={logo} alt="" className="logo" /></Link>
+        <div className="header-right">
+          <PersonOutlineIcon onClick={onClick} style={{ cursor: "pointer" }} />
         </div>
-        <div className="banner">
-            <div className="banner-items">
-                  <p>Get special discount</p>
-                  <h2>up to 85%</h2>
-                  <button>claim voucher</button>
-            </div>
-              <img className="banner-image" src={banner} alt="banner"/>
-        </div>
+      </div>
     </div>
   )
 }
