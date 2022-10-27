b25c4c155cd7b01156a03829fa34a2fd23bd1895
Aje Oluwasegun <ajeoluwasegun18@gmail.com>
2022-10-27 16:09:24 +0100

    fixed the title

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import{Routes, Route} from 'react-router-dom'
 import SignIn from './pages/Signin';
 import SignUp from './pages/Signup';
 import HomePage from './pages/HomePage';
+import ForgotPassword from './pages/ForgotPassword';
 import Profile from './pages/Profile';
 import Settings from './pages/Settings';
 import Favorite from './pages/Favorite';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   
@@ -16,7 +18,10 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/signup' element={<SignUp />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/profile' element={<PrivateRoute />} >
+           <Route path='/profile' element={<Profile/>} />
+          </Route>
         <Route path='/settings' element={<Settings />} />
         <Route path='/favorite' element={<Favorite />} />
       </Routes>
